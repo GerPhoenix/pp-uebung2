64702002f637cc4a110d061d363c4244e17e8dab
Phoenix <34738755+GerPhoenix@users.noreply.github.com>
2019-01-09 10:04:44 +0100

    implementation of the the methods

--- src/io/dama/par/hoh/NewListImpl.java
+++ src/io/dama/par/hoh/NewListImpl.java
@@ -1,79 +1,141 @@
 package io.dama.par.hoh;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class NewListImpl<T> implements NewList<T> {
-	/* Leu'
-		Statt *synchronized* als Schlüsselwort an den Methoden wird hier eine private
-	 	Instanzvariable zum Synchronisieren verwendet, damit niemand von außen an
-	  	derselben Variable einen Lock setzen kann, um Verklemmungen zu vermeiden.
-	 */
-	private final Object intrinsicLock = new Object();
+    /* Leu'
+        Statt *synchronized* als Schlüsselwort an den Methoden wird hier eine private
+         Instanzvariable zum Synchronisieren verwendet, damit niemand von außen an
+          derselben Variable einen Lock setzen kann, um Verklemmungen zu vermeiden.
+     */
 
-	private class ListElement<U> {
-		private U element;
-		private ListElement<U> prev;
-		private final ListElement<U> next;
+    private class ListElement<U> {
+        private final ReentrantLock lock = new ReentrantLock();
+        private U element;
+        private ListElement<U> prev;
+        private ListElement<U> next;
 
-		private ListElement(final U element, final ListElement<U> prev, final ListElement<U> next) {
-			this.element = element;
-			this.prev = prev;
-			this.next = next;
-		}
-	}
+        private ListElement(final U element, final ListElement<U> prev, final ListElement<U> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
 
-	private ListElement<T> first;
+    private final ReentrantLock addLock = new ReentrantLock();
 
-	public NewListImpl() {
-		this.first = null;
-	}
+    private ListElement<T> first;
 
-	/* Sko'
-		Wichtig für das HOH-Lock ist, 
-		dass nicht die gesamte Methode gesperrt wird 
-		sondern bestenfalls nur zwei/drei Elemente der Liste durch den Lock betroffen sind.
-		
-		thisElement.intrinsiclock.lock() bzw thisElement.lock()
-		und / oder
-		ptr.next.intrinsiclock.lock() bzw ptr.next.lock()
-		mit
-		.unlock() des Elements davor.
-		
-		s.a.: https://stackoverflow.com/questions/43762688/java-locks-hand-over-hand-locking-through-list
-	*/
+    public NewListImpl() {
+        first = null;
+    }
+
+    /* Sko'
+        Wichtig für das HOH-Lock ist,
+        dass nicht die gesamte Methode gesperrt wird
+        sondern bestenfalls nur zwei/drei Elemente der Liste durch den Lock betroffen sind.
+
+        lement.intrinsiclock.lock() bzw lement.lock()
+        und / oder
+        ptr.next.intrinsiclock.lock() bzw ptr.next.lock()
+        mit
+        .unlock() des Elements davor.
+
+        s.a.: https://stackoverflow.com/questions/43762688/java-locks-hand-over-hand-locking-through-list
+    */
 	/* Sko'
 	-->	Kann man das Listenobjekt als zu-veränderndes Element *als auch gleichzeitig* als Lock nutzen? <--
 	*/
-	@Override
-	public T get(final int i) {
-		synchronized (this.intrinsicLock) {
-			int j = 0;
-			ListElement<T> ptr = this.first;
-			while (j++ < i) {
-				ptr = ptr.next;
-			}
-			return inspect(ptr.element);
-		}
-	}
+    @Override
+    public T get(final int i) {
+        ListElement<T> ptr;
+        ListElement<T> prev;
+        first.lock.lock();
+        try {
+            if (i == 0) {
+                return inspect(first.element);
+            } else {
+                ptr = first.next;
+                ptr.lock.lock();
+            }
+        } finally {
+            first.lock.unlock();
+        }
+        try {
+            int j = 0;
+            while (j++ < i - 1) {
+                prev = ptr;
+                try {
+                    ptr = ptr.next;
+                    ptr.lock.lock();
+                } finally {
+                    prev.lock.unlock();
+                }
+            }
+            return inspect(ptr.element);
+        } finally {
+            ptr.lock.unlock();
+        }
 
-	@Override
-	public void add(final T e) {
-		synchronized (this.intrinsicLock) {
-			final ListElement<T> insert = new ListElement<>(e, null, this.first);
-			if (this.first != null) {
-				this.first.prev = insert;
-			}
-			this.first = insert;
-		}
-	}
+    }
 
-	@Override
-	public void mod(final int i, final T e) {
-		synchronized (this.intrinsicLock) {
-			int j = 0;
-			ListElement<T> ptr = this.first;
-			while (j++ < i) {
-				ptr = ptr.next;
-			}
-			ptr.element = e;
-		}
-	}
+    @Override
+    public void add(final T e) {
+        ListElement<T> element = new ListElement<>(e, null, null);
+        addLock.lock();
+        try {
+            if (first == null) {
+                first = element;
+            } else {
+                first.lock.lock();
+                ListElement<T> oldFirst = first;
+                try {
+                    element.next = first;
+                    first.prev = element;
+                    first = element;
+                } finally {
+                    oldFirst.lock.unlock();
+                }
+            }
+        } finally {
+            addLock.unlock();
+        }
+
+    }
+
+    @Override
+    public void mod(final int i, final T e) {
+        ListElement<T> ptr;
+        ListElement<T> prev;
+        first.lock.lock();
+        try {
+            if (i == 0) {
+                first.element = e;
+                return;
+            } else {
+                ptr = first.next;
+                ptr.lock.lock();
+            }
+        } finally {
+            first.lock.unlock();
+        }
+        try {
+            int j = 0;
+            while (j++ < i - 1) {
+                prev = ptr;
+                try {
+                    ptr = ptr.next;
+                    ptr.lock.lock();
+                } finally {
+                    prev.lock.unlock();
+                }
+            }
+            ptr.element = e;
+        } finally {
+            ptr.lock.unlock();
+        }
+
+    }
 }
